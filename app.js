1dcbee9f6be2a7f14104fd1b45b5fa588133d4e6
Errorman76 <lwooyoung57@gmail.com>
2021-05-19 01:10:21 +0900

    1. chat 2day

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,18 +40,54 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// socket.io
 app.io = require('socket.io')();
-app.io.on('connection', function(socket){
-  console.log("a user connected");
-  socket.broadcast.emit('hi');
 
-  socket.on('disconnect', function(){
-      console.log('user disconnection');
+app.io.on('connection', function(socket){
+   // 접속한 클라이언트의 정보가 수신되면
+   socket.on('login', function(data) {
+    console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
+
+    // socket에 클라이언트 정보를 저장한다
+    socket.name = data.name;
+    socket.userid = data.userid;
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    io.emit('login', data.name );
   });
 
-  socket.on('chatMessage', function(msg){
-    console.log('message: ' + msg);
-    app.io.emit('chatMessage', msg);
+  // 클라이언트로부터의 메시지가 수신되면
+  socket.on('chat', function(data) {
+    console.log('Message from %s: %s', socket.name, data.msg);
+
+    var msg = {
+      from: {
+        name: socket.name,
+        userid: socket.userid
+      },
+      msg: data.msg
+    };
+
+    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
+    socket.broadcast.emit('chat', msg);
+
+    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
+    // socket.emit('s2c chat', msg);
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    // io.emit('s2c chat', msg);
+
+    // 특정 클라이언트에게만 메시지를 전송한다
+    // io.to(id).emit('s2c chat', data);
+  });
+
+  // force client disconnect from server
+  socket.on('forceDisconnect', function() {
+    socket.disconnect();
+  })
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected: ' + socket.name);
   });
 });
 
