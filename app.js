09d612c34ca13fd94073b4be02d192fb6986eaf3
Errorman76 <lwooyoung57@gmail.com>
2021-05-19 23:26:08 +0900

    1. chat  3day

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var chatRouter = require('./routes/chat');
 
 var app = express();
 
@@ -22,15 +21,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -43,51 +41,15 @@ app.use(function(err, req, res, next) {
 // socket.io
 app.io = require('socket.io')();
 
-app.io.on('connection', function(socket){
-   // 접속한 클라이언트의 정보가 수신되면
-   socket.on('login', function(data) {
-    console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
+app.io.on('connection',(socket) => {
+  console.log('유저가 들어왔다');
 
-    // socket에 클라이언트 정보를 저장한다
-    socket.name = data.name;
-    socket.userid = data.userid;
-
-    // 접속된 모든 클라이언트에게 메시지를 전송한다
-    io.emit('login', data.name );
+  socket.on('disconnect', () => {
+      console.log('유저 나갔다');
   });
 
-  // 클라이언트로부터의 메시지가 수신되면
-  socket.on('chat', function(data) {
-    console.log('Message from %s: %s', socket.name, data.msg);
-
-    var msg = {
-      from: {
-        name: socket.name,
-        userid: socket.userid
-      },
-      msg: data.msg
-    };
-
-    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
-    socket.broadcast.emit('chat', msg);
-
-    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
-    // socket.emit('s2c chat', msg);
-
-    // 접속된 모든 클라이언트에게 메시지를 전송한다
-    // io.emit('s2c chat', msg);
-
-    // 특정 클라이언트에게만 메시지를 전송한다
-    // io.to(id).emit('s2c chat', data);
-  });
-
-  // force client disconnect from server
-  socket.on('forceDisconnect', function() {
-    socket.disconnect();
-  })
-
-  socket.on('disconnect', function() {
-    console.log('user disconnected: ' + socket.name);
+  socket.on('chat-msg', (msg) => {
+    app.io.emit('chat-msg', msg);
   });
 });
 
